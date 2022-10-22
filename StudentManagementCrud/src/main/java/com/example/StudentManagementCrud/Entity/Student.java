e3b7a39b7e49d0455304be8a3b5b835115c109b9
jvcataquiz <jvcataquiz@gmail.com>
2022-10-22 14:56:49 +0800

    injected the annotation to create a table for the database

--- StudentManagementCrud/src/main/java/com/example/StudentManagementCrud/Entity/Student.java
+++ StudentManagementCrud/src/main/java/com/example/StudentManagementCrud/Entity/Student.java
@@ -1,12 +1,21 @@
 package com.example.StudentManagementCrud.Entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="students")
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "first_name", nullable = false)
     private String firstName;
+    @Column(name = "last_name", nullable = false)
     private String lastName;
+    @Column(name = "email", nullable = false)
     private String email;
     public Student() {
-       
+
     }
     public Student(Long id, String firstName, String lastName, String email) {
         this.id = id;
