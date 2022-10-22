498e5af4ff7be130506ca10de9e3e7380c9ee0d7
jvcataquiz <jvcataquiz@gmail.com>
2022-10-22 14:43:36 +0800

    Created packages: entity, service, controller and repository and config application properties for the connection of database to workbencemysql

---
+++ StudentManagementCrud/src/main/java/com/example/StudentManagementCrud/StudentManagementCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudentManagementCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementCrudApplication.class, args);
+	}
+
+}
---
+++ StudentManagementCrud/src/test/java/com/example/StudentManagementCrud/StudentManagementCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudentManagementCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
