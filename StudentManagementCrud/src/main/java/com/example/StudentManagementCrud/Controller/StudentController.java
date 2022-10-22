6b87ed3968ec8f25a0c7a20edbba48d7fb5d9887
jvcataquiz <jvcataquiz@gmail.com>
2022-10-22 15:38:58 +0800

    Using @getmapping annotion to mapp or route the views

--- StudentManagementCrud/src/main/java/com/example/StudentManagementCrud/Controller/StudentController.java
+++ StudentManagementCrud/src/main/java/com/example/StudentManagementCrud/Controller/StudentController.java
@@ -2,6 +2,8 @@ package com.example.StudentManagementCrud.Controller;
 
 import com.example.StudentManagementCrud.Service.StudentService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StudentController {
@@ -10,5 +12,9 @@ public class StudentController {
         this.studentService = studentService;
     }
 //handler method to handle list and return model and view
-
+@GetMapping("/students")
+    public String listStudents(Model model){
+    model.addAttribute("students",studentService.getAllStudents());
+    return "students";
+    }
 }
